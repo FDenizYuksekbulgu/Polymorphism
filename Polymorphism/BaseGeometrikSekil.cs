e00e728041e198e32744ef764f064d6cc5d42cbf
Deniz Yuksekbukgu <fdenizyuksekbulgu@gmail.com>
2025-01-08 23:10:52 +0300

    Add project files.

---
+++ Polymorphism/BaseGeometrikSekil.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+
+namespace Polymorphism
+{
+    public class BaseGeometrikSekil
+    {
+        public int Genislik { get; set; }
+        public int Yukseklik { get; set; }
+        public virtual int Alan(int genislik, int yukseklik)
+        {
+           return genislik * yukseklik;   
+        }
+
+    }
+    //KARE
+    public class Kare:BaseGeometrikSekil
+    {
+        public override int Alan(int genislik, int yukseklik)
+        {
+            return genislik * yukseklik; //Karenin alanı hesaplanır.
+        }
+    }
+    //DİKDÖRTGEN
+    public class Dikdörtgen  : BaseGeometrikSekil
+    {
+        public override int Alan(int genislik, int yukseklik)
+        {
+            return genislik * yukseklik; //Dikdörtgenin alanı hesaplanır.
+        }
+    }
+    //ÜÇGEN
+    public class Üçgen : BaseGeometrikSekil
+    {
+        public override int Alan(int genislik, int yukseklik)
+        {
+            return (genislik + yukseklik) / 2; //Üçgenin alanı hesaplanır.
+        }
+    }
+}
+
+
+
+  
+
+ 
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,24 @@
+﻿using Polymorphism;
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+    //KARE ÖRNEĞİ
+     BaseGeometrikSekil kare = new Kare { Genislik = 4, Yukseklik = 4 };
+    Console.WriteLine($"Karenin alanı: {kare.Alan(kare.Genislik, kare.Yukseklik)}");
+
+    //DİKDÖRTGEN ÖRNEĞİ
+            BaseGeometrikSekil dikdortgen = new Dikdörtgen { Genislik = 5, Yukseklik = 10 };
+    Console.WriteLine($"Dikdörtgenin alanı: {dikdortgen.Alan(dikdortgen.Genislik, dikdortgen.Yukseklik)}");
+
+
+    //ÜÇGEN ÖRNEĞİ
+            BaseGeometrikSekil ucgen = new Üçgen { Genislik = 6, Yukseklik = 8 };
+    Console.WriteLine($"Üçgenin alanı: {ucgen.Alan(ucgen.Genislik, ucgen.Yukseklik)}");
+        }
+
+}
+
+
+
